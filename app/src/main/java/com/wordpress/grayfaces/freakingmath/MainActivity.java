931589c165b1ca90a409036948d71670487942cf
Gray <mrhaidff@gmail.com>
2017-10-16 17:10:27 +0700

    Initial commit

---
+++ app/src/main/java/com/wordpress/grayfaces/freakingmath/MainActivity.java
@@ -0,0 +1,131 @@
+package com.wordpress.grayfaces.freakingmath;
+
+import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
+import com.akexorcist.roundcornerprogressbar.common.BaseRoundCornerProgressBar;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txtLeftNumber,txtRightNumber,txtResultNumber, txtScore,txtcalculus;
+    private Button btnTrue,btnFalse;
+    private RoundCornerProgressBar progressBar;
+    private CountDownTimer countDownTimer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setView();
+    }
+    private void setView(){
+        txtLeftNumber = (TextView) findViewById(R.id.main_txtLeftNumber);
+        txtRightNumber = (TextView) findViewById(R.id.main_txtRightNumber);
+        txtcalculus = (TextView) findViewById(R.id.main_txtCalculus);
+        txtResultNumber = (TextView) findViewById(R.id.main_txtResultNumber);
+        txtScore = (TextView) findViewById(R.id.main_txtScore);
+        btnTrue = (Button) findViewById(R.id.main_btnTrue);
+        btnFalse = (Button) findViewById(R.id.main_btnFalse);
+        progressBar = (RoundCornerProgressBar) findViewById(R.id.main_processBar_timerCountDown);
+        progressBar.setOnProgressChangedListener(new BaseRoundCornerProgressBar.OnProgressChangedListener() {
+            @Override
+            public void onProgressChanged(int viewId, float progress, boolean isPrimaryProgress, boolean isSecondaryProgress) {
+                int percent = ((int) progress)*100/((int)progressBar.getMax());
+                System.out.println(percent);
+                if(percent<=50){
+                    if (percent<=30){
+                        progressBar.setProgressColor(Color.parseColor("#f44336"));
+                    } else {
+                        progressBar.setProgressColor(Color.parseColor("#FF9800"));
+                    }
+                }
+            }
+        });
+        btnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkTheAnswer(true);
+            }
+        });
+        btnFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkTheAnswer(false);
+            }
+        });
+        initNewQuestion();
+    }
+    private void initNewQuestion(){
+        Random random = new Random();
+        int left = random.nextInt(20) + 1;
+        int right = random.nextInt(20) + 1;
+        //20 is the maximum and the 1 is our minimum
+        int calculus = random.nextInt(2);
+        int resultNumber = 0;
+        switch (calculus){
+            case 0:
+                txtcalculus.setText("+");
+                resultNumber = left + right + random.nextInt(2);
+                break;
+            case 1:
+                txtcalculus.setText("-");
+                resultNumber = left - right + random.nextInt(2);
+                break;
+        }
+
+        txtLeftNumber.setText(String.valueOf(left));
+        txtRightNumber.setText(String.valueOf(right));
+        txtResultNumber.setText(String.valueOf(resultNumber));
+        setTimerCountdown();
+    }
+    private void setTimerCountdown(){
+        int time = 10 * 1000;
+        progressBar.setMax(time);
+        progressBar.setProgress(time);
+        progressBar.setProgressColor(Color.parseColor("#8BC34A"));
+        progressBar.setProgressBackgroundColor(Color.parseColor("#757575"));
+        countDownTimer = new CountDownTimer(time,100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                progressBar.setProgress(millisUntilFinished);
+            }
+
+            @Override
+            public void onFinish() {
+                progressBar.setProgress(0);
+                txtScore.setText("0");
+            }
+        };
+        countDownTimer.start();
+    }
+    private void checkTheAnswer(boolean answer){
+        countDownTimer.cancel();
+        countDownTimer = null;
+        int left = Integer.parseInt(txtLeftNumber.getText().toString());
+        int right = Integer.parseInt(txtRightNumber.getText().toString());
+        int rightResult = 0;
+        switch (txtcalculus.getText().toString()){
+            case "+":
+                rightResult = left + right;
+                break;
+            case "-":
+                rightResult = left - right;
+                break;
+        }
+        int result = Integer.parseInt(txtResultNumber.getText().toString());
+        boolean resultAnswer = (result == rightResult);
+        if (answer == resultAnswer){
+            int score = Integer.parseInt(txtScore.getText().toString()) + 1;
+            txtScore.setText(String.valueOf(score));
+        } else {
+            txtScore.setText("0");
+        }
+        initNewQuestion();
+    }
+}
